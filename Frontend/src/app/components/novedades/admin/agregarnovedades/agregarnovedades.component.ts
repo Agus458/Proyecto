0d139d62b424f0dda7e6a8f757cd226e154a8ad6
Darmian407 <damianmesa407@gmail.com>
2021-10-11 18:14:13 -0300

    avances

--- Frontend/src/app/components/novedades/admin/agregarnovedades/agregarnovedades.component.ts
+++ Frontend/src/app/components/novedades/admin/agregarnovedades/agregarnovedades.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Novedad } from 'src/app/models/novedad.model';
+import { NovedadesServicesService } from 'src/app/services/novedades/novedades-services.service';
 
 @Component({
   selector: 'app-agregarnovedades',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarnovedadesComponent implements OnInit {
 
-  constructor() { }
+  novedadesForm: FormGroup = new FormGroup({});
+  constructor(private novedadesservice: NovedadesServicesService, private formBuilder: FormBuilder, private matBar:MatSnackBar) { }
+
 
   ngOnInit(): void {
+    this.novedadesForm = this.formBuilder.group({
+      titulo: ["",[Validators.required]],
+      contenido: ["",[Validators.required]],
+      imagen: ["",[]],
+    });
   }
 
+  ngOnSubmit() {
+    const nuevaNovedad: Novedad = this.novedadesForm.value;
+    this.novedadesservice.postNovedades(nuevaNovedad).subscribe(
+      ok => {
+        this.matBar.open("Novedad creada Correctamente", "Cerrar", {duration: 5000});
+      },
+      error => {
+        this.matBar.open("No se pudo crear la Novedad","Cerrar", {duration: 5000});
+      }
+    );
+    
+  }
+
+
+
+  
 }
